5ac37cbe454cff39dc6c482d3b4d02e7c8299d3f
puneetsehgal <puneetsehgal88@gmail.com>
2020-03-06 11:56:56 -0800

    added functionality

--- creditcard.js
+++ creditcard.js
@@ -1,1 +1,36 @@
 
+window.onload = function(){
+    document.getElementById("card-number").addEventListener("keydown", numbersOnly);
+};
+
+/**
+ * Only allow Numbers for credit card input field
+ *
+ * @function numbersOnly
+ * prevent alphabets and special characters to add in the input field
+ * allow copy, paste and select the input value
+ */
+let numbersOnly = function( event) {
+    console.log(event);
+	if ([46, 8, 9, 27, 13, 110, 190].indexOf(event.keyCode) !== -1 ||
+	// Allow: Ctrl/cmd+A
+	(event.keyCode == 65 && (event.ctrlKey === true || event.metaKey === true)) ||
+	// Allow: Ctrl/cmd+C
+	(event.keyCode == 67 && (event.ctrlKey === true || event.metaKey === true)) ||
+	// Allow: Ctrl/cmd+X
+	(event.keyCode == 88 && (event.ctrlKey === true || event.metaKey === true)) ||
+	// Allow: Ctrl/cmd+V
+	(event.keyCode == 86 && (event.ctrlKey === true || event.metaKey === true)) ||
+	// Allow: home, end, left, right
+	(event.keyCode >= 35 && event.keyCode <= 39)) {
+		return true;
+	}
+	// Ensure that it is a number and stop the keypress
+	if ((event.shiftKey || (event.keyCode < 48 || event.keyCode > 57)) && (event.keyCode < 96 || event.keyCode > 105)) {
+		event.preventDefault();
+		return false;
+	}
+	return true;
+};
+
+
